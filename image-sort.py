9372440eaa8a69ba0bf35c4388860c71b7c1818b
Greenwavemonster <84871889+Greenwavemonster@users.noreply.github.com>
2021-08-07 16:41:59 +0200

    Adapted the rest of the Script
    It worked on my end

--- image-sort.py
+++ image-sort.py
@@ -35,29 +35,31 @@ def userIn():
 
 path1 = r"C:\Users\Greenwave\PycharmProjects\instaread"
 
-def scanEndings(): # This will go into all childfolders to look for images --> .jpg is not changed
-    for users in os.listdir(path1):
-        for image in Path(r'C:\Users\Greenwave\PycharmProjects\instaread', users).glob("*.jpg"):
+def scanEndings():
+    for users in os.listdir(startDir):
+        for image in Path(startDir, users).glob(fileEnd):
             print(image)
             createFolders(users, image)
 
 
-def createFolders(users, image): #It will create a new folder with the same new in the new Directory (Dir should be created if not existent)
-    folderPath = os.path.join(r'C:\Users\Greenwave\Desktop\projectFiles-sorted', users)
-    #print(folderPath)
+def createFolders(users, image):
+    folderPath = os.path.join(endDir, users)
+    print(folderPath)
     if not os.path.exists(folderPath):
         os.makedirs(folderPath)
     else:
-        print("User Exists...")
+        print("User Exists...")  # Yes ik, this will print again and again...
 
     moveImg(folderPath, image)
 
 
-def moveImg(folderPath, image): # Copys things
+def moveImg(folderPath, image):
     shutil.copy(image, folderPath)
-    print("Image has been Copied...")
+    print("File has been Copied...")
 
 
 # === START ===
-print("Start")
+print("=== Start ===")
+userIn()
 scanEndings()
+
